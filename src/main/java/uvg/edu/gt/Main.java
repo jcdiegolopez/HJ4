b0d78b837e6e5972d5dc4c858d853fd56ae53cb4
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-02-26 12:09:48 -0600

    temp

--- src/main/java/uvg/edu/gt/Main.java
+++ src/main/java/uvg/edu/gt/Main.java
@@ -6,35 +6,12 @@ package uvg.edu.gt;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner connIn = new Scanner(System.in);
-        System.out.println("Que implementacion prefiere para el stack?");
-        displayOptions();
-        int op = connIn.nextInt();
-        String stackType = "";
-        switch(op){
-            case 1:
-                stackType = "Array List";
-                break;
-            case 2:
-                stackType = "Vector List";
-                break;
-            case 3:
-                stackType = "Linked List";
-                break;
-            default:
-                System.out.println("Adios.");
-        }
+        
 
 
 
     }
 
-    private static void displayOptions(){
-        System.out.println("1. Array List");
-        System.out.println("2. Vector List.");
-        System.out.println("3. Linked List.");
-        System.out.println("4. Salir.");
-
-    }
+   
 
 }
